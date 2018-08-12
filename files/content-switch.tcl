1fa4a13201c4198c35e93b01297376367e69a66b
Forrest Crenshaw <f.crenshaw@f5.com>
2018-08-12 00:16:00 -0500

    added diagram

--- files/content-switch.tcl
+++ files/content-switch.tcl
@@ -1,14 +1,5 @@
 when HTTP_REQUEST {
     if { ([HTTP::header User-Agent] contains "iphone") or ([HTTP::header User-Agent] contains "Android") }{
-        HTTP::respond 200 content {
-            <html>
-                <head>
-                    <title>iRule Page</title>
-                </head>
-                <body>
-                    Phone user
-                </body>
-            </html>
-        }
+        node 10.1.0.217:8002
     }
 }
